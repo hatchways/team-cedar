b377f0c01ee5f7163df11d05176422b38ae101b1
HaloZod <william.siyang@gmail.com>
2022-01-17 22:02:21 -0500

    update for formating and typing error

--- server/routes/request.js
+++ server/routes/request.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const protect = require("../middleware/auth");
-const { getAllRequest,createRequest,updateRequestById } = require("../controllers/request");
+const { getAllRequest, createRequest, updateRequestById } = require("../controllers/request");
 
-router.route("/").get(protect, getAllRequest).post(protect,createRequest);
+router.route("/").get(protect, getAllRequest).post(protect, createRequest);
 router.route("/:id/edit").put(protect, updateRequestById);
 
 module.exports = router;
