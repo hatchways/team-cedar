97d3dbac7ecee5d413c0320e31c13a34af5b1642
HaloZod <william.siyang@gmail.com>
2022-01-21 11:42:34 -0500

    update bakcend files

--- server/app.js
+++ server/app.js
@@ -13,7 +13,6 @@ const authRouter = require("./routes/auth");
 const userRouter = require("./routes/user");
 const profileRouter = require('./routes/profile');
 const requestRouter = require('./routes/request');
-const stripeRouter = require('./routes/stripe');
 
 const { json, urlencoded } = express;
 
@@ -47,7 +46,7 @@ app.use((req, res, next) => {
 app.use("/auth", authRouter);
 app.use("/users", userRouter);
 app.use("/profile", profileRouter);
-app.use("/request",requestRouter);
+app.use("/request", requestRouter);
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "/client/build")));
