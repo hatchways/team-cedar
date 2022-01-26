042a109956a0c8ddda5f020a57bbc8b134d9569d
jasonfiammetta <jason.fiammetta@gmail.com>
2022-01-26 12:03:49 -0500

    Fixed stripe workflow

--- server/controllers/stripe.js
+++ server/controllers/stripe.js
@@ -1,31 +1,54 @@
 const asyncHandler = require("express-async-handler");
 const stripe = require("stripe")(process.env.STRIPE_SECRET);
 
-// @route GET /connect/stripe
+// @route POST /connect/stripe
 // @desc redirect to Stripe onboarding
 // @access Private
 exports.connect = asyncHandler(async (req, res, next) => {
-  const id = req.user.id;
+  console.log("stripe connecting");
+  const user = await User.findById(req.user.id);
   const site_url =
     process.env.NODE_ENV === "production"
       ? process.env.PRODUCTION_SITE
-      : `http://localhost:${process.env.PORT}`;
+      : "http://localhost:3000";
 
-  const accountLink = stripe.accountLinks
+  stripe.account
     .create({
-      account: `acct_${req.user.id}`,
-      refresh_url: site_url + "/connect/reauth",
-      return_url: site_url + "/connect/return",
-      type: "account_onboarding",
+      type: "standard",
+      business_type: "individual",
+      country: "CA",
+      default_currency: "cad",
+      company: {
+        name: user.name,
+      },
+      email: user.email,
     })
-    .then((res) => {
-      console.log("stripe response", res);
-      if (res.url) {
-        //success
-        res.redirect(301, res.url);
-      } else {
-        res.status(500).json({ error: "Unable to connect to Stripe API" });
-      }
+    .then((userAccount) => {
+      console.log("stripe userAccount", userAccount);
+      stripe.accountLinks
+        .create({
+          account: userAccount.id,
+          refresh_url: site_url + "/settings/payment-methods", // /reauth
+          return_url: site_url + "/settings/payment-methods", // /return
+          type: "account_onboarding",
+        })
+        .then((stripeAccount) => {
+          console.log("stripe response", stripeAccount);
+          if (stripeAccount.url) {
+            //success
+            console.log("redirecting to", stripeAccount.url);
+            res.redirect(stripeAccount.url);
+          } else {
+            res.status(500).json({ error: "Unable to connect to Stripe API" });
+          }
+        })
+        .catch((error) => {
+          console.log(error);
+          next();
+        });
     })
-    .catch(next);
+    .catch((error) => {
+      console.log(error);
+      next();
+    });
 });
--- server/routes/stripe.js
+++ server/routes/stripe.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const router = express.Router();
-const protect = require("../middleware/auth");
+const { protect } = require("../middleware/auth");
 const { connect } = require("../controllers/stripe");
 
-router.route("/stripe").get(protect, connect);
+router.route("/stripe").post(protect, connect);
 
 module.exports = router;
