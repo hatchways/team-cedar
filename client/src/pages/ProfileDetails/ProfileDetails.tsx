fd0c7a2830d4fa9ef5a708b4c0e6e62b33871f0b
Reti Fier <50056219+RetiFier@users.noreply.github.com>
2022-01-31 00:40:20 +0630

    Fix UI and make Responsive

--- client/src/pages/ProfileDetails/ProfileDetails.tsx
+++ client/src/pages/ProfileDetails/ProfileDetails.tsx
@@ -1,137 +1,139 @@
 import * as React from 'react';
-import { Grid, Avatar, Paper, Typography, Container, Button, TextField, Stack, Rating } from '@mui/material';
-import useStyles from './useStyles';
-import DateTimePicker from '@mui/lab/DateTimePicker';
-import LocalizationProvider from '@mui/lab/LocalizationProvider';
-import AdapterDateFns from '@mui/lab/AdapterDateFns';
-import LocationOnIcon from '@mui/icons-material/LocationOn';
-import CreateIcon from '@mui/icons-material/Create';
-import { Formik } from 'formik';
-import * as Yup from 'yup';
+import { Grid, Paper, useMediaQuery, Tab, Tabs } from '@mui/material';
+import { theme } from '../../themes/theme';
+import TabPanel from '../../components/TabPanel/TabPanel';
+import ProfileDetail from '../../components/ProfileDetail/ProfileDetail';
+import ProfileRequest from '../../components/ProfileRequest/ProfileRequest';
+
+const profile = {
+  name: 'Johnathon Smith',
+  descrption: 'Loving Pet Sitter',
+  location: 'Toronto',
+  availability: {
+    day: ['Monday', 'Tuesday', 'Wednesday', 'Thuesday', 'Friday'],
+  },
+  rate: 30,
+  type: 'PetSitter',
+};
 
 export default function ProfileDetails(): JSX.Element {
-  const classes = useStyles();
-  const profile = {
-    firstName: 'Johnathon',
-    lastName: 'Smith',
-    descrption: 'Loving Pet Sitter',
-    location: 'Toronto',
-    availability: 'Monday, Tuesday, Wednesday',
-    about: '',
-    rate: '$30/hr',
+  const [value, setValue] = React.useState(0);
+  const matches = useMediaQuery(theme.breakpoints.down('sm'));
+
+  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+    setValue(newValue);
   };
 
-  const handleSubmit = () => {
-    console.log('todo for an integration ticket');
-  };
+  if (profile?.type !== 'PetSitter') {
+    return (
+      <Grid xs={12} sm={12} md={12} lg={12} item>
+        <Paper
+          elevation={5}
+          sx={{
+            ml: { xs: 5, sm: 5, md: 5, lg: 10 },
+            mr: { xs: 5, sm: 5, md: 5, lg: 10 },
+            padding: { xs: 1, sm: 2, md: 3, lg: 3 },
+            mt: 8,
+            mb: 8,
+          }}
+        >
+          <ProfileDetail
+            name={profile.name}
+            descrption={profile.descrption}
+            location={profile.location}
+            type={profile.type}
+          />
+        </Paper>
+      </Grid>
+    );
+  }
+
   return (
-    <Container className={classes.root}>
-      <Grid
-        container
-        direction="column"
-        md={8}
-        elevation={5}
-        spacing={2}
-        className={classes.profileContainer}
-        component={Paper}
-      >
-        <Grid item>
-          <Avatar alt="Profile Pic" src={`https://robohash.org/test@gmail.com`} className={classes.profilePic} />
+    <Grid container>
+      {matches ? (
+        <Grid xs={12} item>
+          <Tabs value={value} onChange={handleChange} centered sx={{ mt: 3 }}>
+            <Tab label="Profile" />
+            <Tab label="Request" />
+          </Tabs>
+          <TabPanel value={value} index={0}>
+            <Grid xs={12} sm={12} md={7} lg={7} item>
+              <Paper
+                elevation={5}
+                sx={{
+                  ml: { xs: 5, sm: 5, md: 5, lg: 10 },
+                  mr: { xs: 5, sm: 5, md: 5, lg: 0 },
+                  padding: { xs: 1, sm: 2, md: 3, lg: 3 },
+                  mt: 8,
+                  mb: 8,
+                }}
+              >
+                <ProfileDetail
+                  name={profile.name}
+                  descrption={profile.descrption}
+                  location={profile.location}
+                  availability={profile.availability}
+                  type={profile.type}
+                />
+              </Paper>
+            </Grid>
+          </TabPanel>
+          <TabPanel value={value} index={1}>
+            <Grid item xs={12} sm={12} md={5} lg={5}>
+              <Paper
+                elevation={5}
+                sx={{
+                  mb: { xs: 5, md: 10, lg: 10 },
+                  mt: { xs: 5, sm: 5, md: 8, lg: 8 },
+                  mr: { xs: 5, sm: 5, md: 5, lg: 5 },
+                  ml: { xs: 3, sm: 6, md: 5, lg: 10 },
+                  padding: { xs: 1, sm: 0, md: 2, lg: 2 },
+                }}
+              >
+                <ProfileRequest rate={profile.rate} />
+              </Paper>
+            </Grid>
+          </TabPanel>
         </Grid>
-        <Typography align="center" variant="h4" className={classes.name}>
-          {`${profile.firstName} ${profile.lastName}`}
-        </Typography>
-        <Typography align="center" className={classes.description}>
-          {`${profile.descrption}`}
-        </Typography>
-        <Grid item className={classes.locationContainer}>
-          <LocationOnIcon sx={{ color: 'red', marginTop: '8px' }} />
-          <Typography align="center" className={classes.location}>
-            {`${profile.location}`}
-          </Typography>
-        </Grid>
-        <Typography>Availability: {`${profile.availability}`}</Typography>
-        <Grid item className={classes.aboutContainer}>
-          <Typography variant="h6" className={classes.about}>
-            About Me: {`${profile.about}`}
-          </Typography>
-        </Grid>
-        <Grid item className={classes.reviewCountainer}>
-          <Typography color="textPrimary" className={classes.reviews}>
-            Rating and Reviews (0)
-          </Typography>
-        </Grid>
-        <Grid item className={classes.buttonContainer}>
-          <Button variant="contained" startIcon={<CreateIcon />} className={classes.button}>
-            write a review
-          </Button>
-        </Grid>
-      </Grid>
-      <Grid
-        container
-        direction="column"
-        md={4}
-        elevation={5}
-        spacing={1}
-        component={Paper}
-        className={classes.submitFormContainer}
-      >
-        <Grid container direction="column" item spacing={3} className={classes.alignment}>
-          <Grid item>
-            <Typography align="center" variant="h5" className={classes.rate}>
-              {profile.rate}
-            </Typography>
+      ) : (
+        <>
+          <Grid xs={12} sm={12} md={7} lg={7} item>
+            <Paper
+              elevation={5}
+              sx={{
+                ml: { xs: 5, sm: 5, md: 10, lg: 10 },
+                mr: { xs: 5, sm: 5, md: 0, lg: 0 },
+                padding: { xs: 1, sm: 2, md: 3, lg: 3 },
+                mt: 8,
+                mb: 8,
+              }}
+            >
+              <ProfileDetail
+                name={profile.name}
+                descrption={profile.descrption}
+                location={profile.location}
+                availability={profile.availability}
+                type={profile.type}
+              />
+            </Paper>
           </Grid>
-          <Formik
-            initialValues={{
-              dropIn: new Date(),
-              dropOff: new Date(),
-            }}
-            validationSchema={Yup.object().shape({
-              dropIn: Yup.date().required('Please enter a dropin date'),
-              dropOff: Yup.date().required('Please enter a dropoff date'),
-            })}
-            onSubmit={handleSubmit}
-          >
-            {({ handleSubmit, handleChange, values, touched, errors, isSubmitting }) => (
-              <form onSubmit={handleSubmit} noValidate>
-                <LocalizationProvider dateAdapter={AdapterDateFns}>
-                  <Stack spacing={3}>
-                    <Typography align="left" className={classes.titles}>
-                      drop-in
-                    </Typography>
 
-                    <DateTimePicker
-                      value={values.dropIn}
-                      onChange={handleChange}
-                      renderInput={(params) => <TextField {...params} />}
-                    />
-                    <Typography align="left" className={classes.titles}>
-                      drop-off
-                    </Typography>
-
-                    <DateTimePicker
-                      value={values.dropOff}
-                      onChange={handleChange}
-                      renderInput={(params) => <TextField {...params} />}
-                    />
-                  </Stack>
-                </LocalizationProvider>
-                {isSubmitting ? (
-                  <Typography className={classes.titles}> request sent </Typography>
-                ) : (
-                  <Button variant="contained" className={classes.button1}>
-                    send request
-                  </Button>
-                )}
-              </form>
-            )}
-          </Formik>
-          <Button variant="contained" className={classes.button}>
-            message
-          </Button>
-        </Grid>
-      </Grid>
-    </Container>
+          <Grid item xs={12} sm={12} md={5} lg={5}>
+            <Paper
+              elevation={5}
+              sx={{
+                mb: { xs: 5, md: 10, lg: 10 },
+                mt: { xs: 5, sm: 5, md: 8, lg: 8 },
+                mr: { xs: 5, sm: 5, md: 5, lg: 5 },
+                ml: { xs: 3, sm: 6, md: 5, lg: 10 },
+                padding: { xs: 1, sm: 0, md: 2, lg: 2 },
+              }}
+            >
+              <ProfileRequest rate={profile.rate} />
+            </Paper>
+          </Grid>
+        </>
+      )}
+    </Grid>
   );
 }
