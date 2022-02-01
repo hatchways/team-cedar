e658d1c1546a2c35cb360051efbf6f3e422941d3
jasonfiammetta <jason.fiammetta@gmail.com>
2022-02-01 18:58:24 -0500

    Added query string for account type to client's registration request

--- client/src/helpers/APICalls/register.ts
+++ client/src/helpers/APICalls/register.ts
@@ -1,14 +1,14 @@
 import { AuthApiData } from '../../interface/AuthApiData';
 import { FetchOptions } from '../../interface/FetchOptions';
 
-const register = async (name: string, email: string, password: string): Promise<AuthApiData> => {
+const register = async (name: string, email: string, password: string, petSitter: boolean): Promise<AuthApiData> => {
   const fetchOptions: FetchOptions = {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({ name, email, password }),
     credentials: 'include',
   };
-  return await fetch(`/auth/register`, fetchOptions)
+  return fetch(`/auth/register${petSitter ? '?accountType=pet_sitter' : ''}`, fetchOptions)
     .then((res) => res.json())
     .catch(() => ({
       error: { message: 'Unable to connect to server. Please try again' },
