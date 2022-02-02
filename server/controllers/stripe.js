4f4a8cc89885a7a728d617823e280af33e99bcdb
jasonfiammetta <jason.fiammetta@gmail.com>
2022-02-01 19:10:18 -0500

    Added stripe utilities and allowed client requests to create pet sitter accounts

--- server/controllers/stripe.js
+++ server/controllers/stripe.js
@@ -1,6 +1,7 @@
 const asyncHandler = require("express-async-handler");
 const PetSitter = require("../models/PetSitter");
 const stripe = require("stripe")(process.env.STRIPE_SECRET);
+const { createStripeAccountLink } = require("../utils/stripeUtils");
 
 // @route POST /connect/stripe
 // @desc redirect to Stripe onboarding
@@ -9,54 +10,19 @@ exports.connect = asyncHandler(async (req, res, next) => {
   const user = await User.findById(req.user.id);
   const petsitter = await PetSitter.findOne({ userId: user._id });
 
-  if (petsitter.stripeConnectId) {
-    return res
-      .status(403)
-      .json({ error: "Already registered to Stripe Connect" });
+  if (!petsitter) {
+    return res.status(404).json({ error: "Couldn't find Pet Sitter account" });
   }
 
-  const site_url =
-    process.env.NODE_ENV === "production"
-      ? process.env.PRODUCTION_SITE
-      : "http://localhost:3000";
+  const stripeAccount = await createStripeAccountLink(petsitter);
 
-  stripe.account
-    .create({
-      type: "standard",
-      business_type: "individual",
-      country: "CA",
-      default_currency: "cad",
-      company: {
-        name: user.name,
+  if (stripeAccount.url) {
+    res.json({
+      success: {
+        stripeAccount,
       },
-      email: user.email,
-    })
-    .then((userAccount) => {
-      petsitter.stripeConnectId = userAccount.id;
-      petsitter.save();
-      return userAccount;
-    })
-    .then((userAccount) => {
-      return stripe.accountLinks.create({
-        account: userAccount.id,
-        refresh_url: process.env.REFRESH_URL,
-        return_url: process.env.RETURN_URL,
-        type: "account_onboarding",
-      });
-    })
-    .then((stripeAccount) => {
-      if (stripeAccount.url) {
-        res.json({
-          success: {
-            stripeAccount,
-          },
-        });
-      } else {
-        res.status(500).json({ error: "Unable to connect to Stripe API" });
-      }
-    })
-    .catch((error) => {
-      console.log(error);
-      next();
     });
+  } else {
+    res.status(500).json({ error: "Unable to connect to Stripe API" });
+  }
 });
