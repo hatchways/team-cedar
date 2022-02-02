624c699e8cb208acaee3330b3781db2c2bb1fd6e
jasonfiammetta <jason.fiammetta@gmail.com>
2022-02-01 19:10:45 -0500

    Fixed typos

--- server/routes/request.js
+++ server/routes/request.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const protect = require("../middleware/auth");
-const { getAllRequest, createRequest, updateRequestById } = require("../controllers/request");
+const { protect } = require("../middleware/auth");
+const {
+  getAllRequest,
+  createRequest,
+  updateRequestById,
+} = require("../controllers/request");
 
 router.route("/").get(protect, getAllRequest).post(protect, createRequest);
 router.route("/:id/edit").put(protect, updateRequestById);
