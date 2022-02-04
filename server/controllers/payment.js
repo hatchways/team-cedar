12bda8f885a684b446eafa19b7a7dc92847623e8
jasonfiammetta <jason.fiammetta@gmail.com>
2022-02-04 09:38:33 -0500

    Added stripe payment intents to createPayment and makePayment routes

--- server/controllers/payment.js
+++ server/controllers/payment.js
@@ -1,93 +1,119 @@
 const asyncHandler = require("express-async-handler");
 const Payment = require("../models/Payment");
 
-
 exports.createPayment = asyncHandler(async (req, res, next) => {
   const userId = req.user.id;
-  const {sitterId, rate, startTime, endTime, customerId} = req.body;
+  const { sitterId, rate, startTime, endTime, customerId } = req.body;
+
   const createPayment = await Payment.create({
     userId,
     sitterId,
     rate,
     hoursOfService: {
-        startTime,
-        endTime
+      startTime,
+      endTime,
     },
-    customerId
+    customerId,
   });
-  if (!createPayment){
+
+  if (!createPayment) {
     res.status(400);
     throw new Error("Invalid Data");
   }
-  return res.status(200).json({
-      success: {   
-        payment: createPayment 
-      }
-    });
-})
 
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: payment.totalPayment,
+    currency: "cad",
+    payment_method_types: ["card"],
+  });
+
+  console.log("payment intent", paymentIntent);
+  createPayment.client_secret = paymentIntent.client_secret;
+  await createPayment.save();
+
+  return res.status(200).json({
+    success: {
+      payment: createPayment,
+    },
+  });
+});
 
 exports.getPayment = asyncHandler(async (req, res, next) => {
   const { id } = req.params;
   const payment = await Payment.findById(id);
-  if (!payment){
+  if (!payment) {
     res.status(404);
     throw new Error("Pls enter valid payment id");
   }
   return res.status(200).json({
-      success: {   
-        payment: payment 
-      }
-    });
-})
+    success: {
+      payment: payment,
+    },
+  });
+});
 
 exports.makePayment = asyncHandler(async (req, res, next) => {
   const { id } = req.params;
   const userId = req.user.id;
-  const payment = await  Payment.findOne({userId:userId , id});
+  const cardElement = req.body.cardElement;
+  const payment = await Payment.findOne({ userId: userId, id });
   if (!payment) {
     res.status(404);
     throw new Error("Payment doesn't exist");
   }
-  payment.set({paid: true})
-  const updatedPayment = await payment.save()
-  if (updatedPayment){
-   return res.status(200).json({
-      success: {   
-        payment: updatedPayment,
-        msg: "Payment Complete"
-      }
-    });
-  }
-});
 
-exports.cancelPayment =  asyncHandler(async (req, res, next) => {
-  const userId = req.user.id;
-  const { id } = req.params;
-  const merchantPayment = await  Payment.findOne({id: id, sitterId: userId});
-  if (!merchantPayment) {
-    res.status(401);
-    throw new Error("Only merchant can cancel");
-  }  else { 
-    merchantPayment.set(({cancel: true}))
-    const updatedPayment = await merchantPayment.save();
-    if (updatedPayment){
-    return res.status(200).json({
-        success: {   
+  const { paymentIntent, error } = await stripe.confirmCardPayment(
+    payment.client_secret,
+    {
+      payment_method: { card: cardElement },
+    }
+  );
+  if (error) {
+    res.status(500).json({
+      error: {
+        msg: "Unable to complete payment",
+      },
+    });
+  } else {
+    payment.set({ paid: true });
+    const updatedPayment = await payment.save();
+    if (updatedPayment) {
+      return res.status(200).json({
+        success: {
           payment: updatedPayment,
-          msg: "Payment has been Canceled"
-        }
+          msg: "Payment Complete",
+        },
       });
     }
   }
-})
+});
+
+exports.cancelPayment = asyncHandler(async (req, res, next) => {
+  const userId = req.user.id;
+  const { id } = req.params;
+  const merchantPayment = await Payment.findOne({ id: id, sitterId: userId });
+  if (!merchantPayment) {
+    res.status(401);
+    throw new Error("Only merchant can cancel");
+  } else {
+    merchantPayment.set({ cancel: true });
+    const updatedPayment = await merchantPayment.save();
+    if (updatedPayment) {
+      return res.status(200).json({
+        success: {
+          payment: updatedPayment,
+          msg: "Payment has been Canceled",
+        },
+      });
+    }
+  }
+});
 
 exports.getAllPayments = asyncHandler(async (req, res, next) => {
-const payment = await Payment.find({userId: req.user.id});
- return res.status(200).json({
-    success: {   
-      payment: payment    
-    }
+  const payment = await Payment.find({ userId: req.user.id });
+  return res.status(200).json({
+    success: {
+      payment: payment,
+    },
   });
-
-})
+});
